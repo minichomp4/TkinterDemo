fd7796551387c5c329d032af8b8433b384fa8556
Yasemin Turkkan <turkkanyasemin@gmail.com>
2020-11-28 12:59:31 -0800

    Working background image

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+import PIL
 import tkinter
+from tkinter import *
+from PIL import Image, ImageTk
 
+# general setup
 startUp = tkinter.Tk()
-
 startUp.geometry("1280x960")
 startUp.title("Startup Page")
 
+# adding main background
+bgImage = Image.open("StartUpPage1.jpg")
+render = ImageTk.PhotoImage(bgImage)
+img = Label(startUp, image = render)
+img.place(x=0, y=0)
+
+
+# adding buttons
+# button4=tkinter.Button(startUp, text="BOTTOM")
+# button4.pack(side=tkinter.BOTTOM)
+# button3=tkinter.Button(startUp, text="BOTTOM1")
+# button3.pack(side=tkinter.BOTTOM)
+
 startUp.mainloop()
-
-
